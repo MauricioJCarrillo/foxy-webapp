3539ecbbce5e5631d856fb346faf4a6d52e5a7d9
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-04-21 17:28:56 -0500

    Adding ref with useRef to fox image

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
-import { JSX } from "react";
+import { JSX, useRef } from "react";
 
 type RandomFoxProps = {
   image: string;
 };
 
 export const RandomFox = ({ image }: RandomFoxProps): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+
   return (
     <>
       <Image
+        ref={node}
         className="rounded-lg"
         width={320}
         height={320}
