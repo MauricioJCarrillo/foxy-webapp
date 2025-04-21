ba577e1f881076a0aac6ea94122ada21a7207fed
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-04-21 16:20:11 -0500

    Refactoring: Adding type to event handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
 const getRandomFox = (): number => Math.floor(Math.random() * 123) + 1;
@@ -10,40 +10,31 @@ type ImageItem = {
 };
 
 export default function Home() {
-  // const image: string = `https://randomfox.ca/images/${getRandomFox()}.jpg`;
-  // console.log("Image", image);
+  const [images, setImages] = useState<ImageItem[]>([]);
 
-  const [images, setImages] = useState<ImageItem[]>([
-    {
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const newImageItem: ImageItem = {
       id: crypto.randomUUID(),
       url: `https://randomfox.ca/images/${getRandomFox()}.jpg`,
-    },
-  ]);
+    };
 
-  const handleClick = () => {
-    setImages((prevImages) => [
-      ...prevImages,
-      {
-        id: crypto.randomUUID(),
-        url: `https://randomfox.ca/images/${getRandomFox()}.jpg`,
-      },
-    ]);
+    setImages((prevImages) => [...prevImages, newImageItem]);
   };
 
   return (
     <div>
       <h1>Hello World!</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={addNewFox}
+      >
+        Add Random Fox
+      </button>
       {images?.map((image) => (
         <div key={image.id} className="p-2">
           <RandomFox image={image.url} />
         </div>
       ))}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleClick}
-      >
-        Add Random Fox
-      </button>
     </div>
   );
 }
